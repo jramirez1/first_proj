7d7a8645c21cd5cce31cfd1001938f9c61990768
Jose Ramirez <joseramirez2@csumb.edu>
2018-02-07 16:51:36 +0000

    validation screenshots fix

---
+++ Assaignments/Homework1/img/codevalidations/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <div> 
+        
+        <div>Validations</div>
+        <a href="/Assaignments/Homework1/img/codevalidations/codeValidationcss.png">1</a>
+        
+        
+         <a href="/Assaignments/Homework1/img/codevalidations/codeValidation:linux.png">2</a>
+          <a href="/Assaignments/Homework1/img/codevalidations/codeValidation:windows.png" >3</a>
+           <a href="/Assaignments/Homework1/img/codevalidations/codeValidation:mac.png">4</a>
+            <a href="/Assaignments/Homework1/img/codevalidations/codeValidation:index.png">5</a>
+        </div>
+       
+
+    </body>
+</html>
