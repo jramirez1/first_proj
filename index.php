a674e28de6f12fda77a2d084fbc2f912937c51cc
Jose Ramirez <joseramirez2@csumb.edu>
2018-03-12 16:48:33 +0000

    fixed link

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
         <a href="/Labs/portfolio"> portfolio </a>
         <br/>
         <a href="/Labs/777"> lab 2 </a>
+        <br/> 
         <a href="/Silverjack"> lab 3/ silverJack </a>
         </div>
 
