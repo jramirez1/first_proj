98af474452bcbef44d051a5e95b6f2a443dddd6d
Isaac Rodriguez <isarodriguez@csumb.edu>
2018-03-22 00:35:25 +0000

    updated dbconnection code

--- ourstore.php
+++ ourstore.php
@@ -1,19 +1,23 @@
 <?php
 // connect to our mysql database server
 
-function getDatabaseConnection() {
-    $host = "us-cdbr-iron-east-05.cleardb.net";
-    $username = "bb108e997bcdab";
-    $password = "7b071f8b";
-    $dbname = "heroku_3d24ca78bc82e88"; 
-    
-    // Create connection
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    return $dbConn; 
-}
-
+  $connParts = parse_url($url);
+  $connUrl = getenv('JAWSDB_MARIA_URL');
+  $hasConnUrl = !empty($connUrl);
+  
+  $connParts = null;
+  if ($hasConnUrl) {
+      $connParts = parse_url($connUrl);
+  }
+  
+  //var_dump($hasConnUrl);
+  $host = $hasConnUrl ? $connParts['host'] : getenv('IP');
+  $dbname = $hasConnUrl ? ltrim($connParts['path'],'/') : 'dBName';//change dBName to real one later
+  $username = $hasConnUrl ? $connParts['user'] : getenv('C9_USER');
+  $password = $hasConnUrl ? $connParts['pass'] : '';
+  
+  $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+  $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
 <!DOCTYPE html>
